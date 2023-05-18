1b01d2026a0d680cb7ccd72c1eb144494cb3d457
Miguel Huerto <miguelhhuerto@gmail.com>
2023-05-18 16:14:35 +0800

    activity 4: unique number in array

--- activities/activity_4.rb
+++ activities/activity_4.rb
@@ -2,3 +2,17 @@
 #  Write a function that takes an array of ages.
 #  The function will return an Array of ages that you could only see one time.
 #  Print on console the result.
+def unique_ages(ages_array)
+  unique_list=[]
+  ages_array.each do |age|
+    if !(unique_list.include? age)
+      unique_list.push age
+    else
+      unique_list=unique_list-[age]
+    end
+  end
+
+  return unique_list
+end
+
+p unique_ages([31,21,19,24,31,19])
