f292c71c8577ecce079015d8b1167aa5b99ce566
Miguel Huerto <miguelhhuerto@gmail.com>
2023-05-18 15:24:13 +0800

    activity 2: function to join names into complete name

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,13 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+def complete_name(names)
+  fullname=[]
+  names.each do |title,name|
+    fullname<<name
+  end
+  return fullname.join(' ')
+end
+
+p complete_name({first_name:"Miguel Antonio", middle_name: "Habana", last_name: "Huerto"})
