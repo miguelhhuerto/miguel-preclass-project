73a9865dc7730c58f73dc88af8bb53a2c64e5c30
Miguel Huerto <miguelhhuerto@gmail.com>
2023-05-18 16:18:47 +0800

    exercise 3: identify adult age

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -2,7 +2,6 @@ def is_adult(ages)
   # Create new array that will store the adult ages
   adults = []
 
-  # Go through each ages values:
   ages.each do |age|
     # Check if age is greater than or equal 18:
     if age >= 18
